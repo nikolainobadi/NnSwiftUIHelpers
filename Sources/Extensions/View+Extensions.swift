b67efa7242915f89f978db25da18b977d2f4099e
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-08-23 09:10:57 -0700

    added CustomFontViewModifier

--- Sources/Extensions/View+Extensions.swift
+++ Sources/Extensions/View+Extensions.swift
@@ -17,4 +17,3 @@ public extension View {
     /// Percent required in parameter is direct representation. Example: 1% of height = getHeightPercent(1). 10% of height = getHeightPercent(10)
     func getHeightPercent(_ percent: CGFloat) -> CGFloat { screenHeight * (percent * 0.01) }
 }
-

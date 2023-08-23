b0f98dd52a2a5a46a86720d37fa5d46506d90911
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-08-23 11:32:11 -0700

    added ConditionalNavigationLink

---
+++ Sources/ViewModifiers/Conditionals/ConditionalNavigationLink.swift
@@ -0,0 +1,31 @@
+//
+//  ConditionalNavigationLink.swift
+//  
+//
+//  Created by Nikolai Nobadi on 8/23/23.
+//
+
+import SwiftUI
+
+@available(iOS 16.0, *)
+struct ConditionalNavigationLink<D: Hashable>: ViewModifier {
+    let data: D
+    let isActive: Bool
+    
+    func body(content: Content) -> some View {
+        if isActive {
+            NavigationLink(value: data) {
+                content
+            }
+        } else {
+            content
+        }
+    }
+}
+
+@available(iOS 16.0, *)
+extension View {
+    func asNavLink<D: Hashable>(_ data: D, isActive: Bool = true) -> some View {
+        modifier(ConditionalNavigationLink(data: data, isActive: isActive))
+    }
+}

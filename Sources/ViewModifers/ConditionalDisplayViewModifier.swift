53e020f9d0dcc60ee4c7bb699b5a2cab11a7b91d
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-08-23 07:00:00 -0700

    added two conditional view modifiers

---
+++ Sources/ViewModifers/ConditionalDisplayViewModifier.swift
@@ -0,0 +1,24 @@
+//
+//  ConditionalDisplayViewModifier.swift
+//  
+//
+//  Created by Nikolai Nobadi on 8/23/23.
+//
+
+import SwiftUI
+
+struct ConditionalDisplayViewModifier: ViewModifier {
+    let conditional: Bool
+    
+    func body(content: Content) -> some View {
+        if conditional {
+            content
+        }
+    }
+}
+
+extension View {
+    func onlyShow(when conditional: Bool) -> some View {
+        modifier(ConditionalDisplayViewModifier(conditional: conditional))
+    }
+}
---
+++ Sources/ViewModifers/ConditionalModalViewModifier.swift
@@ -0,0 +1,59 @@
+//
+//  ConditionalModalViewModifier.swift
+//  
+//
+//  Created by Nikolai Nobadi on 8/23/23.
+//
+
+import SwiftUI
+
+struct ConditionalModalViewModifier<Modal: View>: ViewModifier {
+    @Binding var isPresented: Bool
+    
+    let modal: Modal
+    let width: CGFloat
+    let height: CGFloat
+    let backgroundColor: Color
+    let cornerRadius: CGFloat
+
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+
+            if isPresented {
+                Color.black.opacity(0.4)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        isPresented = false
+                    }
+
+                VStack(spacing: 0) {
+                    HStack {
+                        Spacer()
+                        Button(action: { isPresented = false }) {
+                            Image(systemName: "xmark")
+                                .padding()
+                                .font(.title)
+                                .foregroundColor(.black)
+                        }
+                    }
+                    
+                    modal
+                }
+                .frame(width: width, height: height)
+                .background(backgroundColor)
+                .cornerRadius(cornerRadius)
+            }
+        }
+    }
+}
+
+extension View {
+    func modal<Modal: View>(isPresented: Binding<Bool>, width: CGFloat = 300, height: CGFloat = 500, backgroundColor: Color = Color(uiColor: .systemBackground), cornerRadius: CGFloat = 20 @ViewBuilder modal: () -> Modal) -> some View {
+        
+        self.modifier(ConditionalModalViewModifier(isPresented: isPresented, width: width, height: height, backgroundColor: backgroundColor, cornerRadius: cornerRadius, modal: modal()))
+    }
+}
+
+
+

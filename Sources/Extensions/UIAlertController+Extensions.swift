92c2fa39538e596589c9559555c4a0db8d20c4c9
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-08-23 17:07:29 -0700

    added extensions for UIAlertController and UIApplication

---
+++ Sources/Extensions/UIAlertController+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  UIAlertController+Extensions.swift
+//  
+//
+//  Created by Nikolai Nobadi on 8/23/23.
+//
+
+import UIKit
+
+public extension UIAlertController {
+    func presentInMainThread(animated: Bool = true, completion: (() -> Void)? = nil) {
+        if var topController = UIApplication.shared.getTopViewController() {
+            while let presentedViewController = topController.presentedViewController {
+                topController = presentedViewController
+            }
+            
+            DispatchQueue.main.async {
+                topController.present(self, animated: animated, completion: completion)
+            }
+        }
+    }
+}

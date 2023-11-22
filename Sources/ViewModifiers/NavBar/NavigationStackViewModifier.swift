0b34a36d7d89cf05c386e4731c7f9eb5aa10eaf7
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-11-22 14:08:01 -0800

    reverted navstack viewmodifer back to normal

--- Sources/ViewModifiers/NavBar/NavigationStackViewModifier.swift
+++ Sources/ViewModifiers/NavBar/NavigationStackViewModifier.swift
@@ -12,8 +12,6 @@ struct NavigationStackViewModifier: ViewModifier {
     var path: Binding<NavigationPath>?
     
     let title: String?
-    let tint: Color
-    let backgroundColor: Color?
 
     func body(content: Content) -> some View {
         if let pathBinding = path {
@@ -25,9 +23,6 @@ struct NavigationStackViewModifier: ViewModifier {
                     content
                 }
             }
-            .tint(tint)
-            .toolbarBackground(backgroundColor == nil ? .automatic : .visible, for: .navigationBar)
-            .toolbarBackground(backgroundColor == nil ? Color.clear : backgroundColor!, for: .navigationBar)
         } else {
             NavigationStack {
                 if let title = title {
@@ -37,16 +32,13 @@ struct NavigationStackViewModifier: ViewModifier {
                     content
                 }
             }
-            .tint(tint)
-            .toolbarBackground(backgroundColor == nil ? .automatic : .visible, for: .navigationBar)
-            .toolbarBackground(backgroundColor == nil ? Color.clear : backgroundColor!, for: .navigationBar)
         }
     }
 }
 
 @available(iOS 16.0, *)
 public extension View {
-    func withNavStack(path: Binding<NavigationPath>? = nil, title: String? = nil, tint: Color = .blue, backgroundColor: Color? = nil) -> some View {
-        self.modifier(NavigationStackViewModifier(path: path, title: title, tint: tint, backgroundColor: backgroundColor))
+    func withNavStack(path: Binding<NavigationPath>? = nil, title: String? = nil) -> some View {
+        self.modifier(NavigationStackViewModifier(path: path, title: title))
     }
 }

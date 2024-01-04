b57cea9892d1fbe8ba4b0c98df35b4ff4f62fdb2
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-01-03 18:37:32 -0800

    modified into dynamic library

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "NnSwiftUIHelpers",
+            type: .dynamic,
             targets: ["NnSwiftUIHelpers"]),
     ],
     dependencies: [],
